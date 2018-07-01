432d4613730ae30e32a1f9d893aa3ead76e92bb3
MinhMinhNguyen <minhnguyenfe96@gmail.com>
2018-07-01 13:24:06 +0700

    Upload du an mai lam

---
+++ Assets/JS/carousel.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        centerMode: true,
+       
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
