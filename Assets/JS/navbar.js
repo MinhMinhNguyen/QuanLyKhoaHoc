ad75fd800528abc4634185cd9997b19168fa9707
tieudiachu <vyduong244@gmail.com>
2018-07-04 14:21:04 +0700

    chinh sua html

--- Assets/JS/navbar.js
+++ Assets/JS/navbar.js
@@ -1,9 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // executes when HTML-Document is loaded and DOM is ready
     $(window).on("scroll", function () {
 
         var scroll = $(window).scrollTop();
-    
+
+        $(".nav-item").click(function () {
+            $("a.active").removeClass('active');
+            $(this).addClass('active');
+           // var active_section = $(this).attr('href'); //get active section id
+            //   $('html, body').animate({
+                    //and scroll to the section
+             //    scrollTop: $(active_section).offset().top }, 1000);
+        });
+
+        var position = $(window).scrollTop();
+        //get header height
+        var header = $('nav').outerHeight();
+
+        //check active section
+        $('.section').each(function (i) {
+            if ($(this).position().top <= (position + header)) {
+                $("a.active").removeClass('active');
+                $("a").eq(i).addClass('active');
+            }
+        });
+
         if (scroll >= 40) {
             $("header").addClass("header-position-fixed");
             $('#top').hide("transition: height 5s, line-height 5s, margin 5s");
@@ -13,26 +34,9 @@ $(document).ready(function() {
         }
     });
     /*end*/
-   // breakpoint and up  
-   $(window).resize(function(){
-       if ($(window).width() >= 980){	
-   
-         // when you hover a toggle show its dropdown menu
-         $(".navbar .dropdown-toggle").hover(function () {
-            $(this).parent().toggleClass("show");
-            $(this).parent().find(".dropdown-menu").toggleClass("show"); 
-          });
-   
-           // hide the menu when the mouse leaves the dropdown
-         $( ".navbar .dropdown-menu" ).mouseleave(function() {
-           $(this).removeClass("show");  
-         });
-     
-           // do something here
-       }	
-   });  
-     
-     
-   
-   // document ready  
-   });
+  
+
+
+
+    // document ready  
+});
